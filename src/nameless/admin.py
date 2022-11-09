4e845afabdd632d9d97fcd1f9af775dc8bc17774
Ionel Cristian Mărieș <contact@ionelmc.ro>
2022-11-09 22:13:24 +0200

    WIP

--- src/nameless/admin.py
+++ src/nameless/admin.py
@@ -1,7 +1,11 @@
+from admin_utils import register_view
 from django.contrib import admin
 from django.contrib.admin import register
+from django_rq import views
 
 from .models import Stuff
+
+register_view(app_label="django_rq", model_name="RQ")(views.stats)
 
 
 @register(Stuff)
--- src/nameless_project/admin.py
+++ src/nameless_project/admin.py
@@ -1,11 +1,6 @@
-import admin_utils
-import django_rq.views
 from django.conf import settings
 from django.contrib.admin import AdminSite
 
 
 class CustomAdminSite(AdminSite):
     site_header = f'Nameless_Project v{settings.PROJECT_VERSION}'
-
-
-admin_utils.register_view(app_label="django_rq", model_name="RQ")(django_rq.views.stats)
