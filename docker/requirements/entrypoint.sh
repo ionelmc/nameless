59a22ea36365107a14b798cb8735d95ccf3053a2
Ionel Cristian Mărieș <contact@ionelmc.ro>
2024-04-11 22:34:04 +0300

    Up.

--- docker/requirements/entrypoint.sh
+++ docker/requirements/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -eux
+python --version
 ls -al requirements
 for req in requirements/*.in; do
   uv pip compile --generate-hashes --allow-unsafe --quiet --upgrade --strip-extras --resolver=backtracking $req --output-file=${req%.*}.txt
