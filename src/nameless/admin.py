44ed5ff1491255cb63179c21d96d7029a2136999
Ionel Cristian Mărieș <contact@ionelmc.ro>
2025-08-25 18:00:09 +0300

    Up.

--- src/nameless/admin.py
+++ src/nameless/admin.py
@@ -1,11 +1,11 @@
 from admin_utils import register_view
 from django.contrib import admin
 from django.contrib.admin import register
-from django_rq import views
+from django_rq.stats_views import stats
 
 from .models import Stuff
 
-register_view(app_label="django_rq", model_name="RQ")(views.stats)
+register_view(app_label="django_rq", model_name="RQ")(stats)
 
 
 @register(Stuff)
